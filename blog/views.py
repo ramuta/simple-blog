f4e9f071b5b6860ce5b7c6501a7b37979f3e284a
Matej Ramuta <matej.ramuta@gmail.com>
2014-09-10 16:54:33 +0100

    add blog post to the database

--- blog/views.py
+++ blog/views.py
@@ -4,17 +4,18 @@ from django.template import RequestContext
 from google.appengine.api import users
 from blog.decorators import login_required
 from blog.forms import BlogPostForm
+from blog.models import BlogPost
 
 
 def blog_render(request, template, params={}):
 	context = RequestContext(request)
 	user = users.get_current_user()
 	if user:
-		logging.info("user exists: " + user.email())
+		#logging.info("user exists: " + user.email())
 		params["logout"] = users.create_logout_url("/")
 		params["email"] = user.email()
 	else:
-		logging.info("user does not exist: ")
+		#logging.info("user does not exist: ")
 		params["login"] = users.create_login_url("/")
 	return render(request=request,
 	              template_name=template,
@@ -23,7 +24,9 @@ def blog_render(request, template, params={}):
 
 
 def home(request):
-	return blog_render(request, "home.html")
+	blog_posts = BlogPost.query().order(-BlogPost.datetime).fetch()
+	params = {"blog_posts": blog_posts}
+	return blog_render(request, "home.html", params)
 
 
 def login(request):
@@ -44,7 +47,11 @@ def post_add(request):
 	elif request.method == "POST":
 		form = BlogPostForm(request.POST)
 
-		if not form.is_valid():
+		if form.is_valid():
+			BlogPost.create(title=form.cleaned_data["title"],
+			                content=form.cleaned_data["content"],
+			                author=users.get_current_user().email())
+		else:
 			params = {"form": form}
 			return blog_render(request, "post_add.html", params)
 
